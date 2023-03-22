422c74a65fa8ddc8272aae53e86804096b3aa8f2
jepietryga <jepi@umich.edu>
2023-03-22 13:04:33 -0400

    Major code

---
+++ classes.py
@@ -0,0 +1,103 @@
+# Defining the needed classes for this to work
+
+
+
+
+class Professor():
+    def __init__(self,
+        name:str,
+        availability_dict,
+        ):
+
+        self.name = name
+        self.availability_dict = availability_dict # This corresponds with the ids of the TimeBlocks
+
+    def available(self,tb:str):
+        tb = str(tb)
+
+        return self.availability_dict[tb]
+
+    @property
+    def availability_metric(self):
+        count = 0
+        for key,val in self.availability_dict.items():
+            if val:
+                count += 1
+        
+        return count
+                                                                                                                                               
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+class Student():
+    def __init__(self,
+        name:str,
+        professor_list:list[str]):
+        self.name = name
+        self._professor_list = professor_list
+        self.professor_dict = {str(professor):False for professor in professor_list} # False: Not met, needs to. TimeBlock: Has met Professor
+
+    def needs_to_meet_professor(self,professor:str):
+        '''
+        Check if the Student needs to meet iwth the professor or doesn't have them listed
+        '''
+        if str(professor) in self.professor_dict:
+            return not self.professor_dict[str(professor)]
+        
+        return False
+    
+    def summary(self):
+        thresh = len(self.professor_dict)
+        count = 0
+        failed_prof = ""
+        for key,val in self.professor_dict.items():
+            if val:
+                count += 1
+            else:
+                failed_prof += key+","
+        
+        return f"{self.name} met with {count}/{thresh} (Didn't Meet: {failed_prof})({self._professor_list})"
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+class TimeBlock():
+    '''
+    Class holds information for which Professors are available AND which students are available
+    '''
+    def __init__(self,
+        id,
+        professor_list:list[Professor],
+        student_list:list[Student]
+        ):
+        self.id = id
+        self.professor_dict = {str(professor):professor.available(id) for professor in professor_list}
+        self.student_dict = {str(student):False for student in student_list}
+
+    def get_professor_student_count(self,professor):
+        count = 0
+        for key,item in professor_dict.items():
+            if item == professor:
+                count += 1
+        
+        return count
+
+    def set_professor_student_match(self,professor,student_list:list[Student]):
+        '''
+        Set students to chosen Professor
+        '''
+
+        for student in student_list:
+            student = str(student)
+            self.student_dict[student] = professor
+
+    def __str__(self):
+        return self.id
+
---
+++ main.py
@@ -0,0 +1,71 @@
+from classes import *
+import json
+import pandas as pd
+
+
+def create_professor_list(path_to_csv):
+    '''
+    Abstracted away, likely loading a Json file or something
+    '''
+    df = pd.read_csv(path_to_csv)
+
+    professor_list = []
+    for ii,row in df.iterrows():
+        
+        prof = Professor(row["Professor"],row.drop("Professor").to_dict())
+        professor_list.append(prof)
+    return professor_list
+
+def create_student_list(path_to_csv):
+    '''
+    Abstractred Awar, likely loading a Json file or something
+    '''
+    df = pd.read_csv(path_to_csv)
+
+    student_list = []
+    for ii,row in df.iterrows():
+        row = row.replace('','NaN')
+        row = row.dropna()
+        student = Student(row["Student"],list(row.drop("Student").to_dict().values()))
+        student_list.append(student)
+
+    return student_list
+
+professor_sort_key = lambda x: x.availability_metric
+
+
+# RunTime Parameters
+num_tb = 5
+k_max = 20
+
+
+# Create Data
+professor_list = create_professor_list("week1_professors.csv")
+student_list = create_student_list("week1_students.csv")
+tb_list = [TimeBlock(f"Meeting {id+1}",professor_list,student_list) for id in range(5)]
+
+print(professor_list)
+print(student_list)
+for student in student_list:
+    print(student.summary())
+print(tb_list)
+# Main run
+
+professor_list = list(sorted(professor_list,key=professor_sort_key)) # Priortize professors with lower availability (NOTE: May need to add a weighting of overlapped timeslots, too)
+
+for professor in professor_list:
+    for tb in tb_list:
+        subset_students = [student for student in student_list if student.needs_to_meet_professor(professor)] # Concern: am I getting the correct students
+
+        if not professor.available(tb):
+            continue
+        
+        grabbed_students = subset_students[0:k_max]
+        tb.set_professor_student_match(professor,grabbed_students)
+        for student in grabbed_students:
+            student.professor_dict[str(professor)] = True # Canuse this to check that students have met every professor at end
+        
+
+for student in student_list:
+    print(student.summary())
+        
